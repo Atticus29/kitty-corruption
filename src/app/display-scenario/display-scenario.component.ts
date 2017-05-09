265e225def7b97543b212933132b7ce0b95e3ece
Mark Fisher and Stormi Hoebelheinrich <stormi@>
2017-05-09 16:30:43 -0700

    got pages to navigate to new scenarios

--- src/app/display-scenario/display-scenario.component.ts
+++ src/app/display-scenario/display-scenario.component.ts
@@ -15,7 +15,7 @@ export class DisplayScenarioComponent implements OnInit {
   scenarioToDisplay;
 
   constructor(private router: Router, private scenarioService: ScenarioService, private catService: CatService) {
-   }
+  }
 
   ngOnInit() {
     this.scenarioToDisplay = this.scenarioService.getScenarioById("0");
@@ -25,13 +25,26 @@ export class DisplayScenarioComponent implements OnInit {
   optionSelected(number: number){
     let scenarioKeyToQuery = this.catService.catChampion.currentScenarioKey;
     let currentUpdateArray = null;
-    // : number[];
-    if(number ===1){
-      this.scenarioService.getScenarioById(scenarioKeyToQuery).subscribe(results=>{
-          currentUpdateArray = results.optionOnePoints;
-          this.catService.catChampion.updateStats(currentUpdateArray);
-      })
-    }
+    this.scenarioService.getScenarioById(scenarioKeyToQuery).subscribe(results=>{
+      if(number ===1){
+        currentUpdateArray = results.optionOnePoints;
+        this.catService.catChampion.updateStats(currentUpdateArray);
+      } else if(number ===2){
+        currentUpdateArray = results.optionTwoPoints;
+        this.catService.catChampion.updateStats(currentUpdateArray);
+      } else if(number ===3){
+        currentUpdateArray = results.optionThreePoints;
+        this.catService.catChampion.updateStats(currentUpdateArray);
+      } else {
+        alert("Something went wrong");
+      }
+      this.scenarioToDisplay = this.scenarioService.getScenarioById(this.catService.catChampion.currentScenarioKey);
+      this.goToScenario(this.catService.catChampion.currentScenarioKey);
+    })
+  }
+  goToScenario(scenarioKey: string){
+    console.log("Got into go to scenario")
+    this.router.navigate(['scenarios', scenarioKey] );
   }
 
 }
