34cd28f01286e13d72ffd2a3617228be90871a1b
Mark Fisher and Stormi Hoebelheinrich <mark@>
2017-05-09 16:05:48 -0700

    use subscribe method to fetch options and update stats

--- src/app/cat.model.ts
+++ src/app/cat.model.ts
@@ -2,10 +2,10 @@ export class Cat {
   currentScenarioKey: string = "0";
   constructor(public captiveName: string, public chosenName: string, public humanTrust: number, public strength: number, public intelligence: number, public imageURL: string) {}
 
-updateStats(newTrust: number, newStrength: number, newIntelligence: number){
-  this.humanTrust += newTrust;
-  this.strength += newStrength;
-  this.intelligence += newIntelligence;
+updateStats(numberArray: number[]){
+  this.humanTrust += numberArray[0];
+  this.strength += numberArray[1];
+  this.intelligence += numberArray[2];
   this.currentScenarioKey = (parseInt(this.currentScenarioKey) + 1).toString();
 }
 
--- src/app/display-scenario/display-scenario.component.ts
+++ src/app/display-scenario/display-scenario.component.ts
@@ -15,12 +15,23 @@ export class DisplayScenarioComponent implements OnInit {
   scenarioToDisplay;
 
   constructor(private router: Router, private scenarioService: ScenarioService, private catService: CatService) {
-    console.log(catService.catChampion);
    }
 
   ngOnInit() {
     this.scenarioToDisplay = this.scenarioService.getScenarioById("0");
     this.catService.getCat();
+  }
+
+  optionSelected(number: number){
+    let scenarioKeyToQuery = this.catService.catChampion.currentScenarioKey;
+    let currentUpdateArray = null;
+    // : number[];
+    if(number ===1){
+      this.scenarioService.getScenarioById(scenarioKeyToQuery).subscribe(results=>{
+          currentUpdateArray = results.optionOnePoints;
+          this.catService.catChampion.updateStats(currentUpdateArray);
+      })
+    }
   }
 
 }
